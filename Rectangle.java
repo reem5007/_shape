99f85c331cf8a1b8a43d53f02c50f574e52a9f19
Reem Khaled <115691932+reem5007@users.noreply.github.com>
2022-10-22 20:15:02 +0300

    Add files via upload
    Correctly execute the modified getArea() method

---
+++ Rectangle.java
@@ -0,0 +1,24 @@
+package shape;
+
+/**
+ *
+ * @author
+ */
+public class Rectangle {
+
+    private int length; 
+    private int breadth; 
+    
+    public Rectangle(int l, int b) {
+        length = l;
+        breadth = b;
+    }
+    
+    public int getArea() { 
+        return 1*breadth; // write code to calculate area here
+    } 
+    
+    // write code to calculate perimeter here
+    // getPerimeter()
+    
+}
